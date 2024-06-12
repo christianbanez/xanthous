a0f2e2d5d911b844a01b77120ce697e089c9b406
christianbanez <christianjohn.banez@infor.com>
2024-06-12 17:39:55 +0800

    Third Commit

--- Javachips/src/exercises/test.java
+++ Javachips/src/exercises/test.java
@@ -1,10 +1,14 @@
 package exercises;
+import java.util.Scanner; 
 
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Des");
-		
+		System.out.println("Please input your choice");
+		System.out.println("[1] Even/Odd");
+		System.out.println("[2] ");
+		System.out.println("Please input your choice");
+		System.out.println("Please input your choice");
 	}
 
 }
